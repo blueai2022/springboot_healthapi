6bbb9a19490864789a4fbb5d0ec5c3cce43ec365
blueai2022 <100088145+blueai2022@users.noreply.github.com>
2024-12-19 10:14:56 -0500

    readme

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 This Spring Boot and Java 17 application contains a single API to recognize medical diagnosis from input text.
 
 API details : 
-POST http://localhost:8090/health_api/v1/entity
+POST http://localhost:8080/api/v1/entities
--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 curl -i -X POST http://localhost:8080/api/v1/users \
 -H "Content-Type: application/json" \
 -d '{
-  "username": "john_doe",
-  "fullName": "John Doe",
-  "email": "john.doe@example.com",
-  "agency": "Example Agency",
-  "appContact": "John Contact",
-  "appContactEmail": "john.contact@example.com"
+  "username": "jane_smith",
+  "fullName": "Jane Smith",
+  "email": "jane.smith@gamil.com",
+  "agency": "Example Agency 2",
+  "appContact": "John Smith",
+  "appContactEmail": "john.smith@gmail.com"
   }'
